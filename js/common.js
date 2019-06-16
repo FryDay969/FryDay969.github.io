38d3b5c5545d2651cc166ce71748ed169f8f9425
Pasha <loging@ukr.net>
2019-06-16 15:14:28 +0300

    3 com

---
+++ js/common.js
@@ -0,0 +1,23 @@
+
+var m = document.getElementById('menubtn1');
+var n = document.getElementById('navbar');
+
+
+
+m.onclick = function(){
+     // n.style.display = 'block';
+   if (n.style.display == 'none'){
+     n.style.display = 'block';
+     
+   }
+  else {
+     n.style.display = 'none';
+   }
+};
+
+
+//$(function() {
+//
+//	// Пользовательские функции
+//
+//});
