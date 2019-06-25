13bbe4dc828ace7bbc6826b02de8beaf000adcd6
Pasha <loging@ukr.net>
2019-06-26 01:20:28 +0300

    19

--- js/common.js
+++ js/common.js
@@ -1,18 +1,16 @@
-
 var m = document.getElementById('menubtn1');
 var n = document.getElementById('navbar');
 
 
 
-m.onclick = function(){
-     // n.style.display = 'block';
-   if (n.style.display == 'none'){
-     n.style.display = 'block';
-     
-   }
-  else {
-     n.style.display = 'none';
-   }
+m.onclick = function () {
+    // n.style.display = 'block';
+    if (n.style.display == 'none') {
+        n.style.display = 'block';
+
+    } else {
+        n.style.display = 'none';
+    }
 };
 
 
